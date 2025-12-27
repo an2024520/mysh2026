b7ea60784822366ff7e0343b5e67c5625759a84b
an2024520 <aaa5461@hotmail.com>
2025-12-27 17:22:50 +0800

    Update sb_patch_warp_ipv6_priority.sh

--- singbox/sb_patch_warp_ipv6_priority.sh
+++ singbox/sb_patch_warp_ipv6_priority.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "gg"
+sleep 5
 # ==============================================================================
 # Script Name: singbox_patch_warp_ipv6_priority.sh
 # Version: v5.8 (Route Strategy Injection)
